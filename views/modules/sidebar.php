0ef59a13d8dce36e04703a55f169c81e48ae9b92
natalie <natalie_loke@hotmail.com>
2020-03-15 17:56:42 +0800

    added inventory in side bar

--- views/modules/sidebar.php
+++ views/modules/sidebar.php
@@ -32,6 +32,15 @@ session_start();
 
                 <ul class="treeview-menu menu-open treeview-menu-visible">
                     <li>
+                        <a href="insights-inventory">
+                            <i class="fa fa-circle-o"></i>
+                            <span>Inventory</span>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul class="treeview-menu menu-open treeview-menu-visible">
+                    <li>
                         <a href="insights-example">
                             <i class="fa fa-circle-o"></i>
                             <span>Example</span>
