7e53a149b0c40d191f3b0b490bd7dd88ff6a5254
azlinda <e0191267@u.nus.edu>
2020-03-21 21:53:44 +0800

    test code for sales

--- controllers/sales.controller.php
+++ controllers/sales.controller.php
@@ -2,4 +2,12 @@
 
 class SalesController
 {
+    public static function ctrViewAllSales()
+    {
+        $response = ItemKitModel::mdlViewAllSales();
+
+        return $response;
+    }
+
+
 }
--- models/sales.model.php
+++ models/sales.model.php
@@ -4,4 +4,16 @@ require_once "connection.php";
 
 class SalesModel
 {
+    public static function mdlViewAllSales()
+    {
+        $stmt = Connection::connect()->prepare("SELECT sum(quantity_purchased) as totalQuantity, sum(item_unit_price) as totalPrice, totalQuantity*totalPrice as totalSales, sales.sale_time FROM sales_items JOIN sales ON sales_items.sale_id = sales.sale_id GROUP BY sales.sale_time");
+        $stmt->execute();
+
+        return $stmt->fetch();
+
+        $stmt->close();
+        $stmt = null;
+    }
+
+    
 }
